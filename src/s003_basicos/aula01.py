18093b30228908bca5198dba25fb0545a774e849
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2023-05-06 04:47:16 -0300

    Comentarios

---
+++ src/s003_basicos/aula01.py
@@ -0,0 +1,11 @@
+"""
+DocString - Não é um comentário, o Python lê isso,
+mas muitos utilizam como comentário
+"""
+
+''' Usar para escrever notas (o Python também lê essa linha)'''
+
+# Exibe uma informação na saída padrão
+print("Olá Mundo")                      # Imprimindo string
+print(123)                              # Imprimindo números
+print("Olá", "Mundo", "Separado")       # Imprimindo strings concatenadas e separadas por espaço
